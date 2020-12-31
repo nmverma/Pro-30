a74ba2476f9edf9cafb95cbc8d3b339e5b83b89c
nmverma <74199409+nmverma@users.noreply.github.com>
2020-12-31 20:41:55 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,27 @@
+class Block {
+  constructor(x, y, width, height) {
+    var options = {
+     'restitution ':0.2,
+      'friction' :0.0
+
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+   // strokeWeight(2);
+    //stroke("green");
+   // fill("skyblue");
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
